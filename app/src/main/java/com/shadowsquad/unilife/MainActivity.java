f80fbcaeb682383bc9c51df5ccb9782fed665f67
Lakshan Vidanapathirana <rlvidanapathirana@gmail.com>
2021-04-22 00:39:53 +0530

    Created bottom nav bar

---
+++ app/src/main/java/com/shadowsquad/unilife/MainActivity.java
@@ -0,0 +1,62 @@
+package com.shadowsquad.unilife;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
+
+
+
+        BottomNavigationView bottomNavigationView1 =findViewById(R.id.bottom_nav);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HomeFragment()).commit();
+
+
+    }
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            Fragment selectedFragment=null;
+            switch (item.getItemId())
+            {
+                case R.id.home:
+                selectedFragment= new HomeFragment();
+                break;
+
+                case R.id.exam:
+                    selectedFragment= new ExamFragment();
+                    break;
+
+                case R.id.event:
+                    selectedFragment= new EventFragment();
+                    break;
+
+                case R.id.lecture:
+                    selectedFragment= new LectureFragment();
+                    break;
+            }
+
+getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
+            return true;
+        }
+    };
+
+
+}
