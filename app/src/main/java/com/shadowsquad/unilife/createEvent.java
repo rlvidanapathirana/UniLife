300b8d76a30e33bced54b0fd7e02463e30634a88
Asanka Gamage <asankagamage1998@gmail.com>
2021-04-28 22:34:18 +0530

    creted event create page

--- app/src/main/java/com/shadowsquad/unilife/createEvent.java
+++ app/src/main/java/com/shadowsquad/unilife/createEvent.java
@@ -1,14 +1,44 @@
 package com.shadowsquad.unilife;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EdgeEffect;
+import android.widget.ImageButton;
 
 public class createEvent extends AppCompatActivity {
-
+    ImageButton btn1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_event);
+
+        btn1 = (ImageButton) findViewById(R.id.backarrow);
+        btn1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction fragmentTransaction=getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container,new EventFragment()).commit();
+            }
+        });
+
+        //back button
+//        ImageButton btn = (ImageButton) findViewById(R.id.backarrow);
+//
+//        btn.setOnClickListener(new View.OnClickListener(){
+//            public void onClick(View v){
+//                startActivity(new Intent( createEvent.this,EventFragment.class));
+//            }
+//        });
+
+
+
+
+
     }
 }
