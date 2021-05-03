c86a93c50b9e9d84c6a1cad12c985481aa60beda
tuanrimzan <tuan.rimzan421@gmail.com>
2021-05-03 23:53:35 +0530

    upto gpa calculator

--- app/src/main/java/com/shadowsquad/unilife/HomeFragment.java
+++ app/src/main/java/com/shadowsquad/unilife/HomeFragment.java
@@ -4,16 +4,41 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class HomeFragment extends Fragment {
+
+    Button btnCal;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home,container,false);
+        View view = inflater.inflate(R.layout.fragment_home,container,false);
+
+        btnCal = (Button) view.findViewById(R.id.btnCalc);
+        btnCal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(android.view.View v) {
+                calcSelect calcselect = new calcSelect() ;
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container,calcselect);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
+
+
+
+
+
+
+        return view;
     }
 }
---
+++ app/src/main/java/com/shadowsquad/unilife/calcSelect.java
@@ -0,0 +1,24 @@
+package com.shadowsquad.unilife;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link calcSelect#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class calcSelect extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_calc_select, container, false);
+    }
+}
