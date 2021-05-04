ab8be33d64a9761365fc5b255169ff4d5a115d34
tuanrimzan <tuan.rimzan421@gmail.com>
2021-05-04 05:40:50 +0530

    all the fragments related to gpa calculator function is created

--- app/src/main/java/com/shadowsquad/unilife/calcSelect.java
+++ app/src/main/java/com/shadowsquad/unilife/calcSelect.java
@@ -3,10 +3,13 @@ package com.shadowsquad.unilife;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,10 +18,40 @@ import android.view.ViewGroup;
  */
 public class calcSelect extends Fragment {
 
+    Button gpaCal, cgpaCal;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_calc_select, container, false);
+        View view = inflater.inflate(R.layout.fragment_calc_select, container, false);
+
+        gpaCal = (Button) view.findViewById(R.id.btngpa);
+        cgpaCal = (Button) view.findViewById(R.id.btncgpa);
+        gpaCal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(android.view.View v) {
+                gpaCalc gpacalc = new gpaCalc();
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.linearLayout1,gpacalc);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
+
+        cgpaCal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(android.view.View v) {
+                cgpaCalc cgpacalc = new cgpaCalc();
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.linearLayout1,cgpacalc);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
+
+        return view;
     }
 }
---
+++ app/src/main/java/com/shadowsquad/unilife/gpaCalc.java
@@ -0,0 +1,24 @@
+package com.shadowsquad.unilife;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link gpaCalc#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class gpaCalc extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_gpa_calc, container, false);
+    }
+}
