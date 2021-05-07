742eff4b0adea7d353d8de068be87f8990ca8b10
pawanlakshan <pawanlakzan97@gmail.com>
2021-05-08 02:38:02 +0530

    lecture pages updated

--- app/src/main/java/com/shadowsquad/unilife/ExamFragment.java
+++ app/src/main/java/com/shadowsquad/unilife/ExamFragment.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
@@ -14,25 +13,9 @@ import androidx.fragment.app.Fragment;
 
 public class ExamFragment extends Fragment {
 
-    //Button addBtnExam;
-
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-       View View = inflater.inflate(R.layout.fragment_exam,container,false);
-
-        ImageButton BtnOpenExam = (ImageButton) View.findViewById(R.id.BtnOpenExam);
-        BtnOpenExam.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(android.view.View v) {
-                Intent in = new Intent(getActivity(),AddExam.class);
-                startActivity(in);
-            }
-        });
-
-
-        return View;
-        }
-
+        return inflater.inflate(R.layout.fragment_exam,container,false);
     }
-
+}
